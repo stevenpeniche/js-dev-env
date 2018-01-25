e371b24f5b0e0bad682883f05db842879f200c0d
Steven Peniche <stevenmpeniche@gmail.com>
2018-01-24 21:25:38 -0500

    Add 'JSON Schema Server & Mock Data Api'

---
+++ src/api/userApi.js
@@ -0,0 +1,21 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUrl'
+
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+  return get('users');
+}
+
+// Functions below are private
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json()
+}
+
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
